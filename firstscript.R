f6f2de1505f3141d47b088e65a9061568aa9e1d7
francois-sissoko <fsissoko007@yahoo.com>
2018-01-23 15:50:46 -0700

    First couple of commits for R

---
+++ firstscript.R
@@ -0,0 +1,13 @@
+#Set working directory
+#setwd("C:/~/Documents/firstscript.R")
+
+#Load required libraries 
+#library(dplyr)
+#library(ggplot2)
+
+#Create some objects
+x <- 5
+y <- 6
+z <- x - y
+
+print(z)
